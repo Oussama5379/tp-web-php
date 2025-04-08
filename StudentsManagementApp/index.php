1c34e9b809d79bd068dd351ad2d9b60108fb5a99
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-04-08 19:24:15 +0100

    Create the first page of the studenst management app

---
+++ StudentsManagementApp/index.php
@@ -0,0 +1,38 @@
+<?php 
+    include "fragments/header.php"
+?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Students Management System</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Liste des étudiants</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Liste des sections</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Logout</a>
+      </li>
+      
+    </ul>
+    
+  </div>
+</nav>
+
+<h1> HELLO ,PHP LOVERS ! Welcome to you Adminstration Platform</h1>
+
+
+
+
+
+
+
+<?php include 'fragments/footer.php' ?>

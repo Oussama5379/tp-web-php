235a27f855e6878deab0763dcef75891cb6a7ef5
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-04-09 11:23:33 +0100

    Create a class that contains the operations of each table in the DataBase

---
+++ StudentsManagementApp/class/Section.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Section {
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+    public function getListOfSections()
+    {
+        $query = 'Select * from section ';
+        $response = $this->_db->query($query);
+        return $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function getListOfStudentsBySection($section){
+        $query ='SELECT St.id ,St.name , St.birthday ,St.image 
+                FROM Student St , Section Se where Se.designation=:section and St.sectionId == Se.id  ';
+        $response = $this->db->prepare($query);
+        $response->execute(['section' => $section]);
+        $students = $response->fetchAll(PDO::FETCH_ASSOC);
+        return $students;
+    }
+
+
+    
+
+    
+};
---
+++ StudentsManagementApp/class/Student.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Student {
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function  getStudentById( $id){
+        $query ='SELECT St.id ,St.name , St.birthday ,St.image ,Se.designation as section
+                FROM Student St , Section Se where St.id=:stId and St.sectionId == Se.id  ';
+        $response = $this->db->prepare($query);
+        $response->execute(['stId' => $id]);
+        $student = $response->fetch(PDO::FETCH_ASSOC);
+        return $student;
+    }
+    public function getAllStudents(){
+        $query ='SELECT St.id ,St.name , St.birthday ,St.image ,Se.designation as Section
+                FROM Student St , Section Se where St.sectionId == Se.id  ';
+        $response = $this->db->query($query);
+        $students = $response->fetchAll(PDO::FETCH_ASSOC);
+        return $students;
+
+    }
+    
+};
